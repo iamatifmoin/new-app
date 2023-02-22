04df0d9ef17294b038a16f67db239b106faadeb8
deshdeepakkushwaha <deshdeepakkushwaha04@gmail.com>
2023-02-22 15:48:30 +0530

    home page changed

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,20 +1,27 @@
 import BlurredCircles from "../components/BlurredCircles";
 import "../styles/_home.scss";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
-    <div className="home">
+    <div className="index">
+      <Navbar />
       <section className="hero">
         <div className="hero-body">
-          <h1 className="title">Name</h1>
+          <h1 className="heading">Welcome to Daily Bread</h1>.
+          <img src="./images/food.jpg" alt="Food" className="foodImg"></img>
+          <p className="title">
+            Every portion of Food is very precious. We promise to deliver this
+            precious stuff to needy person
+          </p>
+          <div className="btns-container">
+            <button className="btn-large">I Want to Donate Foods</button>
+            <button className="btn-large"> I want to Received Foods</button>
+          </div>
         </div>
-        {/* <div className="btns-container">
-          <a className="button">Student Registration</a>
-
-          <a className="button">Mentor Registration</a>
-        </div> */}
 
         <BlurredCircles />
+
         <div style={{ width: "100%", backgroundColor: "black" }}></div>
       </section>
     </div>
