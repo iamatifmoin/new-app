36a74d459847cb4526b397aca838cd596e6574fa
iamatifmoin <atifmoin3@gmail.com>
2023-02-23 20:36:07 +0530

    added routing in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,61 @@
 import React from "react";
 // import "./Navbar.css";
 import "../styles/_navbar.scss";
+import {
+  Outlet,
+  Link,
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import About from "../views/About";
+import Contact from "../views/Contact";
+import Login from "../views/Login";
+import Signup from "../views/Signup";
 
 function Navbar() {
   return (
-    <nav>
-      {/* <div className="hamburger">
+    <>
+      <nav>
+        {/* <div className="hamburger">
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
       </div> */}
-      <div className="nav-outer">
-        <div className="nav-links_left">
-          <button href="#" className="btn-one">
-            Home
-          </button>
-          <button href="#" className="btn-one">
-            About
-          </button>
-          <button href="#" className="btn-one">
-            Contact Us
-          </button>
-        </div>
-        <div className="nav-links_right">
-          <button className="login-button btn-two" href="#">
-            Login
-          </button>
-          <button className="join-button btn-two" href="#">
-            Join
-          </button>
-        </div>
-      </div>
-    </nav>
+        <Router>
+          <div className="nav-outer">
+            <div className="nav-links_left">
+              <Link to="/" className="btn-one">
+                Home
+              </Link>
+              <Link to="/about" className="btn-one">
+                About
+              </Link>
+              <Link to="/contact" className="btn-one">
+                Contact Us
+              </Link>
+            </div>
+            <div className="nav-links_right">
+              <Link className="login-button btn-two" to="/login">
+                Login
+              </Link>
+              <Link className="join-button btn-two" to="/signup">
+                Join
+              </Link>
+            </div>
+            <Routes>
+              {/* <Route index element={<Home />}></Route> */}
+              <Route exact path="about" element={<About />}></Route>
+              <Route exact path="contact" element={<Contact />}></Route>
+              <Route exact path="login" element={<Login />}></Route>
+              <Route exact path="signup" element={<Signup />}></Route>
+              {/* <Route exact path="*" element={<Nopage />}></Route> */}
+            </Routes>
+          </div>
+        </Router>
+      </nav>
+      <Outlet />
+    </>
   );
 }
 
---
+++ src/views/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import Navbar from "../components/Navbar";
+
+const About = () => {
+  return (
+    <div>
+      {/* <div style={{ width: "100vw", height: "100vh", backgroundColor: "red" }}>
+        AboutPage
+      </div> */}
+    </div>
+  );
+};
+
+export default About;
