6c2acb40efc6092a4d46412d97f8d5413648b3df
iamatifmoin <atifmoin3@gmail.com>
2023-03-04 20:27:44 +0530

    setting up api keys

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+require("dotenv").config();
 root.render(
   <React.StrictMode>
     <BrowserRouter>
