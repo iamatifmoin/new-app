4248836ce780a02d7ab291bd40baad276c660c9e
iamatifmoin <atifmoin3@gmail.com>
2023-03-06 10:39:11 +0530

    removing warnings

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,22 +8,38 @@ function Footer() {
         <div className="left">
           <p>Desh Deepak Kushwaha</p>
           <div className="social">
-            <a href="https://www.linkedin.com/in/desh-deepak-kushwaha-b65929254/">
-              <div class="icons8-linkedin"></div>
+            <a
+              href="https://www.linkedin.com/in/desh-deepak-kushwaha-b65929254/"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              <div className="icons8-linkedin"></div>
             </a>
-            <a href="https://github.com/DeshDeepakKushwaha">
-              <div class="icons8-github"></div>
+            <a
+              href="https://github.com/DeshDeepakKushwaha"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              <div className="icons8-github"></div>
             </a>
           </div>
         </div>
         <div className="right">
           <p>Atif Moin</p>
           <div className="social">
-            <a href="https://www.linkedin.com/in/atifmoin/">
-              <div class="icons8-linkedin"></div>
+            <a
+              href="https://www.linkedin.com/in/atifmoin/"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              <div className="icons8-linkedin"></div>
             </a>
-            <a href="https://github.com/iamatifmoin">
-              <div class="icons8-github"></div>
+            <a
+              href="https://github.com/iamatifmoin"
+              target={"_blank"}
+              rel={"noreferrer"}
+            >
+              <div className="icons8-github"></div>
             </a>
           </div>
         </div>
